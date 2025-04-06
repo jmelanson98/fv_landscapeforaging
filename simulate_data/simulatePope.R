6850b775858c15a88ca72387be1a15a9b4d488db
jmelanson98 <jenna.melanson@ubc.ca>
2025-04-06 11:15:15 -0700

    adding some pre-run ideas

--- simulate_data/simulatePope.R
+++ simulate_data/simulatePope.R
@@ -5,9 +5,11 @@
 
 ##### Load packages #####
 library(matrixStats)
+library(sp)
+library(gstat)
 
 ##### Load source code #####
-source("/simulation/sim_src")
+source("simulate_data/sim_src.R")
 
 ##### Required Data #####
 ### number of colonies (num_col)
@@ -60,39 +62,78 @@ theta = 1
 ##### Define colony characteristics #####
 col_id = 1:num_col
 colsize = rep(num_bees/num_col, num_col)
-col_xcoord = runif(num_col, colony_lower, colony_upper)
-col_ycoord = runif(num_col, colony_lower, colony_upper)
+col_xcoord = round(runif(num_col, colony_lower, colony_upper)) #colony locations are integers (e.g., round to nearest meter)
+col_ycoord = round(runif(num_col, colony_lower, colony_upper)) #colony locations are integers (e.g., round to nearest meter)
 
 colony_data = as.data.frame(cbind(col_id, colsize, col_xcoord, col_ycoord))
 
 #plot colonies
 landscape = plot(colony_data$col_xcoord, colony_data$col_ycoord)
 
-##### Define trap characteristics #####
+##### Define trap locations #####
 trap_id = 1:num_traps
-trap_quality = rnorm(num_traps, 0, 1)
 trap_xcoord = runif(num_traps, trap_lower, trap_upper)
 trap_ycoord = runif(num_traps, trap_lower, trap_upper)
-
-trap_data = as.data.frame(cbind(trap_id, trap_quality, trap_xcoord, trap_ycoord))
+trap_data = as.data.frame(cbind(trap_id, trap_xcoord, trap_ycoord))
 
 #plot traps
 landscape = landscape + plot(trap_data$trap_xcoord, trap_data$trap_ycoord, col = "red")
 
+##### Define floral landscape #####
+# use Brownian variogram to simulate spatial distribution of resources (as in Pope & Jha)
+# this block of code (until "Simulate Data") modified from ChatGPT suggestion
+
+# create a regular grid of points to simulate over
+x.range <- seq(colony_lower, colony_upper, by = 2)  # X coordinates
+y.range <- seq(colony_lower, colony_upper, by = 2)  # Y coordinates
+
+grid <- expand.grid(x = x.range, y = y.range)
+coordinates(grid) <- ~x + y
+gridded(grid) <- TRUE
+
+# define a Brownian variogram model
+# "nugget" = 0, "sill" = total variance
+vgm_model <- vgm(psill = 1, model = "Lin", nugget = 0, range = 1) #range is not actually used in a Lin model but we need it as a place holder so gstat doesn't get mad
+
+# simulate one realization of a Gaussian random field
+simulated <- gstat(formula = z ~ 1, locations = ~x + y, dummy = TRUE, 
+                   beta = 0, model = vgm_model, nmax = 20)
+
+simulated_field <- predict(simulated, newdata = grid, nsim = 1) #change nsim to make multiple landscapes
+# this takes a little while for a landscape of my size ... 
+
+# plot the simulated resource distribution
+spplot(simulated_field, zcol = "sim1", main = "Simulated Resource Distribution (Brownian variogram)")
+
+# OR
+r <- raster(simulated_field["sim1"])
+plot(r, main = "Brownian Variogram (Linear) Simulation")
+
 
 ##### Simulate data #####
+
+# create lambda_full -> a landscape-wide visitation table (visitation rate between each pair of pixels based on distance and floral quality)
+
+# start = starting location (xs, ys)
+# end = ending location (xe, ye, fe) (fqe = floral quality)
+
+lambda_full = allocMatrix(nrow = length(colony_lower:colony_upper), ncol = length(colony_lower:colony_upper), value = 0)
+
+
+
 distance_mat = allocMatrix(nrow = num_col, ncol = num_traps, value = 0)
 lambda = allocMatrix(nrow = num_col, ncol = num_traps, value = 0)
+ypred = allocMatrix(nrow = num_col, ncol = num_traps, value = 0)
 
 for (k in trap_data$trap_id){
   for (i in colony_data$col_id){
-    distance_mat = dist
-    lambda = 
-      
-      dis[i, k] <- distance(delta[i], trap[k]); #is distance a function in stan??
-    lambda[i, k] <- -beta*dis[i, k] + theta*floral[k] + mu + zeta_scale[i] + eps_scale[k];
+    distance_mat[i, k] = calculate_dist(colony_coords = list(colony_data$col_xcoord[i], colony_data$col_ycoord[i]),
+                                  trap_coords = list(trap_data$trap_xcoord[k], trap_data$trap_ycoord[k]))
+    lambda[i, k] = exp(-1/inv_beta*distance_mat[i, k] + theta*trap_data$trap_quality[k])
     
+    #draw trap number using eqn 3
+    
+    ypred[i, k] = rpois(1, lambda[i, k])
   }
 }
-lambda = exp(-1/inv_beta)
 
