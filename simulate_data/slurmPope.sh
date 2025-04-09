fa7468e16030a4369e131afb72eeae87908918fc
jmelanson98 <jenna.melanson@ubc.ca>
2025-04-08 20:10:06 -0700

    modify bash script

--- simulate_data/slurmPope.sh
+++ simulate_data/slurmPope.sh
@@ -6,10 +6,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=4G
-#SBATCH --time=06:00:00
-#SBATCH --account=def-youraccount  # Replace with your Alliance account
-#SBATCH --mail-user=your@email.com
-#SBATCH --mail-type=END,FAIL
+#SBATCH --time=12:00:00
 
 module load r/4.3.1  # Adjust version as needed
 
