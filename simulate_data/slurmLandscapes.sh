fc0d74ff2d6cda81c0803f03cb9f5ca325548a74
jmelanson98 <jenna.melanson@ubc.ca>
2025-04-10 19:37:31 -0700

    bash script for simulating landscapes

---
+++ simulate_data/slurmLandscapes.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=landscape_sim
+#SBATCH --output=logs/sim_%A_%a.out
+#SBATCH --error=logs/sim_%A_%a.err
+#SBATCH --array=1-10
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=4G
+#SBATCH --time=1:00:00
+
+module load StdEnv/2023 gcc r/4.3.1 gdal proj
+
+# List of required packages
+packages=("matrixStats" "sp" "gstat" "ggplot2" "reshape2" "raster" "rasterVis" "parallel" "future" "furrr")
+
+
+# Loop through the list of packages and install them if missing
+for package in "${packages[@]}"; do
+  Rscript -e "if (!require('$package')) install.packages('$package', repos='https://cloud.r-project.org/')"
+done
+
+Rscript parallelizeSimulateLandscape.R
