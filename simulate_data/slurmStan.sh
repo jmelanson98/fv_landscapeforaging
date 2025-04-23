e23b1af50494fcfd2295c1c4ca58d733a42906be
Jenna Melanson <melanson@cedar1.int.cedar.computecanada.ca>
2025-04-22 18:51:52 -0700

    small edits to slurm script

--- simulate_data/slurmStan.sh
+++ simulate_data/slurmStan.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=fit_stan
 #SBATCH --output=logs/stan_%A_%a.out
 #SBATCH --error=logs/stan_%A_%a.err
-#SBATCH --array=121-150
+#SBATCH --array=1-150
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=8G
