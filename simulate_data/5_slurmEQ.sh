025c9c76c8c6a0d126807f133ccf77c5eb8e30f5
jmelanson98 <jenna.melanson@ubc.ca>
2025-05-09 19:25:57 -0700

    more sims

--- simulate_data/5_slurmEQ.sh
+++ simulate_data/5_slurmEQ.sh
@@ -2,11 +2,11 @@
 #SBATCH --job-name=bee_sim
 #SBATCH --output=logs/sim_%A_%a.out
 #SBATCH --error=logs/sim_%A_%a.err
-#SBATCH --array=1-150
+#SBATCH --array=1-120
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=16G
-#SBATCH --time=12:00:00
+#SBATCH --time=2:00:00
 
 module load StdEnv/2023 gcc r/4.3.1 gdal proj
 
