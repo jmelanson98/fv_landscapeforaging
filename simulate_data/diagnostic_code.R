bfa3d6394807f8e833510ae20f6ca480c33f6a25
jmelanson98 <jenna.melanson@ubc.ca>
2025-05-11 23:01:39 -0700

    add some diagnostics code from Mike Betancourt

--- simulate_data/diagnostic_code.R
+++ simulate_data/diagnostic_code.R
@@ -3,18 +3,37 @@
 # Started by: Jenna Melanson
 # This code is modified from various tidbits from Michael Betancourt (https://betanalpha.github.io)
 
+#source
+source("simulate_data/mcmc_analysis_tools_rstan.R")
+
+# extract expectands
+extract_expectands <- function(stan_fit) {
+  nom_params <- rstan:::extract(stan_fit, permuted=FALSE)
+  N <- dim(nom_params)[3] - 1
+  params <- lapply(1:N, function(n) t(nom_params[,,n]))
+  names(params) <- names(stan_fit)[1:N]
+  (params)
+}
+
+extract_hmc_diagnostics <- function(stan_fit) {
+  diagnostic_names <- c('divergent__', 'treedepth__', 'n_leapfrog__', 
+                        'stepsize__', 'energy__', 'accept_stat__')
+  
+  nom_params <- get_sampler_params(stan_fit, inc_warmup=FALSE)
+  C <- length(nom_params)
+  params <- lapply(diagnostic_names, 
+                   function(name) t(sapply(1:C, function(c) 
+                     nom_params[c][[1]][,name])))
+  names(params) <- diagnostic_names
+  (params)
+}
 
 # check distribution of divergent transitions in parameter space
-partition <- partition_div(fit_cp)
-div_params_cp <- partition[[1]]
-nondiv_params_cp <- partition[[2]]
-
-par(mar = c(4, 4, 0.5, 0.5))
-plot(nondiv_params_cp$'theta[1]', log(nondiv_params_cp$tau),
-     col=c_dark, pch=16, cex=0.8, xlab="theta.1", ylab="log(tau)",
-     xlim=c(-20, 50), ylim=c(-6,4))
-points(div_params_cp$'theta[1]', log(div_params_cp$tau),
-       col="green", pch=16, cex=0.8)
+x = c("mu", "beta", "theta")
+y = c("mu", "beta", "theta")
+samples = extract_expectands(stanFitGQ)
+diagnostics = extract_hmc_diagnostics(stanFitGQ)
+plot_div_pairs(x, y, samples, diagnostics)
 
 
 stan_rhat
