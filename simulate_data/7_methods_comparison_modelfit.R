26c282897e5c41a447e79a71e27f2dd2b4c43413
jmelanson98 <jenna.melanson@ubc.ca>
2025-05-27 00:16:51 -0700

    finish simulation scripts

--- simulate_data/7_methods_comparison_modelfit.R
+++ simulate_data/7_methods_comparison_modelfit.R
@@ -1,4 +1,5 @@
 ##### Accuracy comparison of different simulation/modeling scenarios #####
+##### THIS SCRIPT IS FOR THE MODEL FITTING AND PLOTTING PHASE ######
 # Script Initiated: May 26, 2025
 # By: Jenna Melanson
 # Goals:
@@ -36,80 +37,18 @@ setwd("/home/melanson/projects/def-ckremen/melanson/fv_landscapeforaging") # ser
 options(mc.cores = parallel::detectCores())
 
 
-
-
 ##### Source functions #####
 source("simulate_data/src/GeneralizedSimFunctions.R")
 
 
-##### Create a grid of parameters to test #####
-if (file.exists("simulate_data/methods_comparison/param_grid.rds")){
-  param_grid = readRDS("simulate_data/methods_comparison/param_grid.rds")
-} else{
-  landscape_ids = 1:10
-  rho <- c(50, 75, 100, 125, 150)
-  colony_density <- c(8000) # using this because it's 4 * sample_size
-  sample_sizes <- c(2000)
-  distance_decay = c("exponentiated_quadratic", "exponential")
-  model_approach = c("all", "singletons", "doubletons", "centroid")
-  
-  param_grid <- expand.grid(
-    landscape_id = landscape_ids,
-    rho = rho,
-    sample_size = sample_sizes,
-    distance_decay = distance_decay,
-    model_approach = model_approach,
-    stringsAsFactors = FALSE
-  )
-  param_grid$true_average_foraging = NA
-  param_grid$true_sd_foraging = NA
-  param_grid$model_average_foraging = NA
-  param_grid$model_sd_foraging = NA
-  param_grid$counts = NA
-  param_grid$num_unobserved = NA
-}
-
-
-
-
-##### Simulate data ######
-# Get task ID from SLURM environment variable
+##### Load in param grid and simulated data #####
+param_grid = readRDS("simulate_data/methods_comparison/param_grid.rds")
 task_id <- as.integer(Sys.getenv("SLURM_ARRAY_TASK_ID"))
-params <- param_grid[task_id, ]
+inputfilepath <- sprintf("simulate_data/methods_comparison/data/sim_result_%03d", task_id)
+yobs = readRDS(paste(inputfilepath, "/yobs.RDS", sep = ""))
+colony_data = readRDS(paste(outfilepath, "/colonydata.RDS", sep = ""))
+trap_data = readRDS(paste(outfilepath, "/trapdata.RDS", sep = ""))
 
-# Get landscape from saved file
-fq <- readRDS(paste0(sprintf("landscapes/random_field_range10_large/landscape_%03d", params$landscape_id), ".rds"))
-
-# Run simulation
-result <- draw_bees_colony_restricted(
-  sample_size     = params$sample_size,
-  landscape_size  = 1500,
-  colonygrid_size = 700,
-  trapgrid_size   = 300,
-  number_traps    = 25,
-  number_colonies = params$colony_density,
-  colony_sizes    = rep(100, params$colony_density),
-  rho            = params$rho,
-  theta           = 0.5,
-  resource_landscape = fq,
-  nesting_landscape = NULL,
-  distance_decay = params$distance_decay
-)
-
-# Save output
-outfilepath <- sprintf("simulate_data/methods_comparison/data/sim_result_%03d", task_id)
-dir.create(outfilepath, recursive = TRUE, showWarnings = FALSE)
-saveRDS(result[[1]], paste(outfilepath, "/yobs.RDS", sep = ""))
-saveRDS(result[[2]], paste(outfilepath, "/colonydata.RDS", sep = ""))
-saveRDS(result[[3]], paste(outfilepath, "/trapdata.RDS", sep = ""))
-
-#save coloony metrics to param_grid
-nonzero = yobs[rowSums(yobs) > 0,]
-zero = yobs[rowSums(yobs) ==0,]
-param_grid$counts = list(rowSums(nonzero))
-param_grid$num_unobserved = nrow(zero)
-param_grid$true_average_foraging = mean(result[[2]]$foraging_range)
-param_grid$true_sd_foraging - sd(result[[2]]$foraging_range)
 
 ##### Fit Models ######
 
@@ -124,9 +63,6 @@ data$priorVa = 1
 data$priorCo = 1
 data$rho_center = 4.5
 data$rho_sd_log = 0.5
-
-# Save original yobs
-yobs.orig = yobs
 
 ### Correctly subset yobs based on approach
 if (model_approach == "all"){
@@ -222,7 +158,7 @@ summary$technique = paste(summary$distance_decay, summary$model_approach, sep = 
 
 #### Accuracy plot
 fig = ggplot(summary, aes(x = true_avg, y = model_avg,
-                   color = technique)) +
+                          color = technique)) +
   geom_point() +
   geom_errorbar(aes(ymin = model_avg - 2*model_sd, ymax = model_avg + 2*model_sd)) +
   geom_abline(slope = 1, intercept = 0) +
