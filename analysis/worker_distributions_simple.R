ba9abd7d98fad7586c30be230e69c583e2e8672a
jmelanson98 <jenna.melanson@ubc.ca>
2025-05-30 14:41:00 -0700

    literally panicking, whyyyy are there so many sibships from different sites it makes no biological sense :'(

--- analysis/worker_distributions_simple.R
+++ analysis/worker_distributions_simple.R
@@ -41,9 +41,9 @@ samplePoints = as.data.frame(read.table("/Users/jenna1/Documents/UBC/bombus_proj
 colnames(samplePoints) = c("sample_pt", "gps","landowner","subsite")
 
 # sibships
-mix2022 = read.csv("data/siblingships/mix_sibships_preliminary_2022.csv")
-mix2023 = read.csv("data/siblingships/mix_sibships_preliminary_2023.csv")
-imp2022 = read.csv("data/siblingships/imp_sibships_preliminary_2022.csv")
+mix2022 = read.csv("data/siblingships/mix_sibships_final_2022.csv")
+mix2023 = read.csv("data/siblingships/mix_sibships_final_2023.csv")
+imp2022 = read.csv("data/siblingships/imp_sibships_final_2022.csv")
 imp2023 = read.csv("data/siblingships/imp_sibships_preliminary_2023.csv")
 
 
@@ -52,8 +52,8 @@ imp2023 = read.csv("data/siblingships/imp_sibships_preliminary_2023.csv")
 ######################
 
 # merge 2022 + 2023 specimens
-allspecs = rbind(specimenData2022[, colnames(specimenData2022) %in% c("site", "round", "sample_pt", "sample_id", "year", "barcode_id", "active_flower", "final_id", "notes", "pollen")],
-                 specimenData2023[, colnames(specimenData2023) %in% c("site", "round", "sample_pt", "sample_id", "year", "barcode_id", "active_flower", "final_id", "notes", "pollen")]) %>%
+allspecs = rbind(specimenData2022[, colnames(specimenData2022) %in% c("site", "round", "sample_pt", "sample_id", "year", "barcode_id", "active_flower", "final_id", "notes", "pollen", "plate")],
+                 specimenData2023[, colnames(specimenData2023) %in% c("site", "round", "sample_pt", "sample_id", "year", "barcode_id", "active_flower", "final_id", "notes", "pollen", "plate")]) %>%
   filter(final_id == "B. mixtus" | final_id == "B. impatiens")
 
 #add lat and long to samplePoints
@@ -78,7 +78,7 @@ allsibs = rbind(mix2022[, colnames(mix2022) %in% tokeep],
 allspecs = left_join(allspecs, allsibs, by = c("barcode_id" = "OffspringID"))
 genotypedspecs = filter(allspecs, !is.na(ClusterIndex))
 
-#split 2022 and 2023
+#split species
 mix = genotypedspecs %>% filter(final_id == "B. mixtus")
 imp = genotypedspecs %>% filter(final_id == "B. impatiens")
 
@@ -111,6 +111,7 @@ ggsave("figures/manuscript_figures/impatiensmap_bothyears.jpg", impgrids,
 # initiate vector to save pairwise distance values
 mix_centroid = c()
 imp_centroid = c()
+points_centroids = c()
 
 # loop through sib pairs (or triplets, or quadruplets...) and record distances
 for(sibship in unique(nonsingletonmix$ClusterIndex)){
@@ -140,4 +141,55 @@ ggplot(as.data.frame(imp_centroid), aes(x = imp_centroid)) +
   labs(title = "B. impatiens", y = "count", x = "distance from sibship centroid (m)") +
   theme_minimal()
 
+
+# for all trap locations
+df_sf <- st_as_sf(samplePoints, coords = c("long", "lat"), crs = 4326)
+dist_matrix <- st_distance(df_sf)
+dist_matrix <- as.matrix(dist_matrix)
+
+# for trap locations restricted by site
+sf_df <- samplePoints %>%
+  mutate(group_letter = substr(sample_pt, 1, 1)) %>%
+  st_as_sf(coords = c("long", "lat"), crs = 4326)
+
+# Function to compute pairwise distances within each group
+group_dists <- sf_df %>%
+  group_split(group_letter) %>%
+  map_dfr(function(group) {
+    if (nrow(group) < 2) return(NULL)
+    
+    combn(nrow(group), 2, simplify = FALSE) %>%
+      map_dfr(~{
+        idx1 <- .x[1]
+        idx2 <- .x[2]
+        dist <- st_distance(group[idx1, ], group[idx2, ]) |> as.numeric()
+        tibble(
+          pt1 = group$sample_pt[idx1],
+          pt2 = group$sample_pt[idx2],
+          group_letter = group$group_letter[idx1],
+          distance_m = dist
+        )
+      })
+  })
+
+
 write.csv(allspecs, "data/siblingships/allsibships_cleaned.csv")
+
+
+
+### check number of sib pairs in same vs different sites
+sum = nonsingletonmix %>% group_by(ClusterIndex) %>% summarize(n = n(), 
+                                                               num_sites = length(unique(site)),
+                                                               plate1 = plate[1],
+                                                               plate2 = plate[2])
+differentsites = sum %>% filter(num_sites > 1)
+plates = as.data.frame(c(differentsites$plate1, differentsites$plate2))
+colnames(plates) = c("plates")
+platesum_falsesibs = plates %>% group_by(plates) %>% summarize(nfalse=n())
+
+samesite = sum %>% filter(num_sites == 1)
+plates = as.data.frame(c(samesite$plate1, samesite$plate2))
+colnames(plates) = c("plates")
+platesum_truesibs = plates %>% group_by(plates) %>% summarize(ntrue=n())
+
+platesum = full_join(platesum_falsesibs, platesum_truesibs)
