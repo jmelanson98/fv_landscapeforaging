7810adb77492c9c53c873358814dd4ed99ffed51
jmelanson98 <jenna.melanson@ubc.ca>
2025-07-16 16:21:23 -0700

    slightly simpler model + tighter prior on deltas

--- models/colonydetectionprior.stan
+++ models/colonydetectionprior.stan
@@ -64,41 +64,41 @@ zeta ~ normal(0, 1);
 for(k in 1:K){
   for(i in 1:C){
     dis[i, k] = sqrt(square(delta[i, 1] - trap[k,1]) + square(delta[i, 2] - trap[k,2]));
-    lambda[i, k] = dis[i,k]/(-rho*exp(theta*floral[k])) + mu + zeta_scale[i] + eps_scale[k];
+    lambda[i, k] = (-dis[i,k]/rho) + theta*floral[k] + mu + zeta_scale[i] + eps_scale[k];
     y[i, k] ~ poisson_log(lambda[i, k]);
   } 
 }
 }
 
-generated quantities {
-  vector[C] colony_dist;        // Declare estimated colony foraging distance
-  colony_dist = rep_vector(0, C);  // Initialize colony distance to 0
-  
-  // start anonymous scope
-  {
-    matrix[C,K] dis; //distance from colony C to trap K
-    matrix[C,K] lambda; //rate of captures for colony C at trap K
-    vector[C] V;     // Declare local variable for each colony's total visitation
-    real alpha = 1e-12;  // set smaaaaall number to prevent division by 0
-    
-    // Recompute lambda and dis
-    for(k in 1:K){
-      for(i in 1:C){
-        dis[i, k] = sqrt(square(delta[i, 1] - trap[k,1]) + square(delta[i, 2] - trap[k,2]));
-        lambda[i, k] = dis[i,k]/(-rho*exp(theta*floral[k])) + mu + zeta_scale[i] + eps_scale[k];
-      } 
-    }
-    
-    // Compute V for normalization
-    for (i in 1:C){
-      V[i] = sum(exp(lambda[i,]));
-    }
-    
-    
-    // compute colony_dist to be saved outside the anonymous scope
-    for (k in 1:K){
-      colony_dist = colony_dist + (dis[,k] .* exp(lambda[,k]) ./ (V + alpha));
-    }
-  }
-}
-
+// generated quantities {
+//   vector[C] colony_dist;        // Declare estimated colony foraging distance
+//   colony_dist = rep_vector(0, C);  // Initialize colony distance to 0
+//   
+//   // start anonymous scope
+//   {
+//     matrix[C,K] dis; //distance from colony C to trap K
+//     matrix[C,K] lambda; //rate of captures for colony C at trap K
+//     vector[C] V;     // Declare local variable for each colony's total visitation
+//     real alpha = 1e-12;  // set smaaaaall number to prevent division by 0
+//     
+//     // Recompute lambda and dis
+//     for(k in 1:K){
+//       for(i in 1:C){
+//         dis[i, k] = sqrt(square(delta[i, 1] - trap[k,1]) + square(delta[i, 2] - trap[k,2]));
+//         lambda[i, k] = dis[i,k]/(-rho*exp(theta*floral[k])) + mu + zeta_scale[i] + eps_scale[k];
+//       } 
+//     }
+//     
+//     // Compute V for normalization
+//     for (i in 1:C){
+//       V[i] = sum(exp(lambda[i,]));
+//     }
+//     
+//     
+//     // compute colony_dist to be saved outside the anonymous scope
+//     for (k in 1:K){
+//       colony_dist = colony_dist + (dis[,k] .* exp(lambda[,k]) ./ (V + alpha));
+//     }
+//   }
+// }
+// 
--- simulate_data/12_colony_detection_prior.R
+++ simulate_data/12_colony_detection_prior.R
@@ -21,7 +21,8 @@ library(dplyr)
 library(tidyr)
 library(gridExtra)
 library(tibble)
-library(ggpubr)
+#library(ggpubr)
+library(terra)
 
 ##### Set Environment #####
 setwd("/Users/jenna1/Documents/UBC/bombus_project/fv_landscapeforaging") # local
@@ -32,31 +33,31 @@ options(mc.cores = parallel::detectCores())
 source("simulate_data/src/GeneralizedSimFunctions.R")
 
 ##### Simulate some data #####
-# use 2000 background colonies and 1000 observations?
-# this distribution won't be totally accurate but it's hard to fit a model with 8000 background colonies
-
-# Get landscape from saved file
-fq <- readRDS("simulate_data/landscapes/landscapes/random_field_range10/landscape_001.rds")
-fq = terra::rast(fq)
-
-# Run simulation
-result <- draw_bees_colony_restricted(
-  sample_size     = 1000,
-  landscape_size  = 1500,
-  colonygrid_size = 700,
-  trapgrid_size   = 300,
-  number_traps    = 25,
-  number_colonies = 2000,
-  colony_sizes    = rep(100, 2000),
-  rho            = 50,
-  theta           = 0.5,
-  resource_landscape = fq,
-  nesting_landscape = NULL,
-  distance_decay = "exponential"
-)
+# # use 2000 background colonies and 1000 observations?
+# # this distribution won't be totally accurate but it's hard to fit a model with 8000 background colonies
+# 
+# # Get landscape from saved file
+# fq <- readRDS("simulate_data/landscapes/landscapes/random_field_range10/landscape_001.rds")
+# #fq = terra::rast(fq)
+# 
+# # Run simulation
+# result <- draw_bees_colony_restricted(
+#   sample_size     = 1000,
+#   landscape_size  = 1500,
+#   colonygrid_size = 700,
+#   trapgrid_size   = 300,
+#   number_traps    = 25,
+#   number_colonies = 2000,
+#   colony_sizes    = rep(100, 2000),
+#   rho            = 50,
+#   theta           = 0.5,
+#   resource_landscape = fq,
+#   nesting_landscape = NULL,
+#   distance_decay = "exponential"
+# )
 
 # Save results
-saveRDS(result, "simulate_data/methods_comparison/observed_vs_unobserved/simdata.rds")
+#saveRDS(result, "simulate_data/methods_comparison/observed_vs_unobserved/simdata.rds")
 result = readRDS("simulate_data/methods_comparison/observed_vs_unobserved/simdata.rds")
 
 # Write outputs to variables
@@ -75,7 +76,7 @@ data$K = 25
 data$trap = as.matrix(cbind(trap_data$trap_x, trap_data$trap_y))
 data$lowerbound = 0
 data$upperbound = 1500
-data$nestrange = 175
+data$nestrange = 100
 data$floral = trap_data$fq
 data$priorVa = 1
 data$priorCo = 1
@@ -90,6 +91,59 @@ stanfile = paste("models/colonydetectionprior.stan")
 stanFitCPrior = stan(file = stanfile,
                              data = data, seed = 5838299,
                              chains = 4, cores = 4,
-                             iter = 2000,
+                             iter = 4000, warmup = 1000,
                              verbose = TRUE)
-saveRDS(stanFit, file="simulate_data/methods_comparison/observed_vs_unobserved/stanFitAll.RDS")
+saveRDS(stanFitCPrior, file="simulate_data/methods_comparison/observed_vs_unobserved/stanFitCPrior.RDS")
+
+
+
+# Plot the posteriors of some colonies
+#Plot the posteriors of ten colonies
+plot_list = list()
+numplots = 6
+legends = list()
+colony_observed = colony_data[rowSums(yobs) > 0,]
+
+for (i in 1:numplots){
+  c_id = colony_observed$colonyid[i]
+  delta_draws = as.data.frame(rstan::extract(stanFitAll_widerlimit, pars = "delta")$delta[, c_id,])
+  colnames(delta_draws) = c("x","y")
+  trap_data$trap_count = yobs_detected[c_id, ]
+  
+  p = ggplot(delta_draws, aes(x = x, y = y)) +
+    geom_density_2d_filled(alpha = 0.8) +
+    
+    #plot colony location
+    geom_point(data = colony_observed[c_id,], aes(x = colony_x, y = colony_y), colour = "lightgreen", show.legend = TRUE) +
+    
+    #plot trap locations / sizes / quality
+    geom_point(data = trap_data, aes(x = trap_x, y = trap_y, size = trap_count, colour = fq)) +
+    scale_colour_gradient(low = "white", high = "red") +
+    scale_size_continuous(limits = c(0,10), range = c(1, 5)) +
+    
+    #miscellaneous
+    ggtitle(paste("Colony", c_id)) +
+    labs(title = paste("Colony", c_id), 
+         size = "Number of Captures", 
+         gradient = "Trap Floral Quality",
+         level = "Colony Location & Posterior Distribution") +
+    coord_equal() +
+    theme_minimal()
+  
+  # save legend
+  #g <- ggplotGrob(p)
+  #legend_index <- which(g$layout$name == "guide-box-right")
+  #legend <- g$grobs[[legend_index]]
+  
+  # remove legend from plot
+  #p <- p + theme(legend.position = "none")
+  
+  #save plot
+  plot_list[[i]] = p
+  #legends[[1]] = legend
+}
+
+fig = grid.arrange(grobs = plot_list, ncol = 3)
+#fig = grid.arrange(fig, legends[[1]], ncol = 2, widths = c(4,1))
+ggsave("figures/simfigs/colonyposteriors_detectionprior.png", fig, height = 750, width = 1500)
+
