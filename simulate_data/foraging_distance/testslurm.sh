31ce3f3d895a59dbda58454d2a3f9ea60777f6f4
Jenna Melanson <melanson@gra-login2.graham.sharcnet>
2025-07-29 00:55:23 -0400

    save COLONY runs with sibship prior and proper simulation methodology

--- simulate_data/foraging_distance/testslurm.sh
+++ simulate_data/foraging_distance/testslurm.sh
@@ -2,11 +2,11 @@
 ##SBATCH --job-name=test
 #SBATCH --output=methods_comparison/landscape_effects/logs/landscapesensitivity_%A_%a.out
 #SBATCH --error=methods_comparison/landscape_effects/logs/landscapesensitivity_%A_%a.err
-#SBATCH --array=1
+#SBATCH --array=1-360
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=8G
-#SBATCH --time=00:30:00
+#SBATCH --time=06:00:00
 
 module load StdEnv/2023 gcc r/4.3.1 gdal proj
 
