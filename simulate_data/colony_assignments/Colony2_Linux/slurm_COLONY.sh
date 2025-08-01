2733c92576bbc8789b5ae4068437a8ed1839a86f
jmelanson98 <jenna.melanson@ubc.ca>
2025-08-01 11:08:33 -0700

    rename some files

--- simulate_data/colony_assignments/Colony2_Linux/slurm_COLONY.sh
+++ simulate_data/colony_assignments/Colony2_Linux/slurm_COLONY.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=colony_run
-#SBATCH --array=0-47
+#SBATCH --array=0-11
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=8G
 #SBATCH --time=12:00:00
@@ -11,13 +11,12 @@
 module load gcc
 
 # Your list of .DAT files (replace with real names or glob dynamically)
-FILES=(mixtus_set3_sub1.DAT mixtus_set3_sub0.8.DAT mixtus_set3_sub0.6.DAT mixtus_set3_sub0.4.DAT
-mixtus_set3_sub0.2.DAT mixtus_set3_sub0.1.DAT mixtus_set3_sub0.05.DAT mixtus_set3_sub0.025.DAT
-mixtus_set2_sub1.DAT mixtus_set2_sub0.8.DAT mixtus_set2_sub0.6.DAT mixtus_set2_sub0.4.DAT
-mixtus_set2_sub0.2.DAT mixtus_set2_sub0.1.DAT mixtus_set2_sub0.05.DAT mixtus_set2_sub0.025.DAT
-mixtus_set1_sub1.DAT mixtus_set1_sub0.8.DAT mixtus_set1_sub0.6.DAT mixtus_set1_sub0.4.DAT
-mixtus_set1_sub0.2.DAT mixtus_set1_sub0.1.DAT mixtus_set1_sub0.05.DAT mixtus_set1_sub0.025.DAT
-impatiens_set3_sub1.DAT impatiens_set3_sub0.8.DAT impatiens_set3_sub0.6.DAT impatiens_set3_sub0.4.DATimpatiens_set3_sub0.2.DAT impatiens_set3_sub0.1.DAT impatiens_set3_sub0.05.DAT impatiens_set3_sub0.025.DATimpatiens_set2_sub1.DAT impatiens_set2_sub0.8.DAT impatiens_set2_sub0.6.DAT impatiens_set2_sub0.4.DATimpatiens_set2_sub0.2.DAT impatiens_set2_sub0.1.DAT impatiens_set2_sub0.05.DAT impatiens_set2_sub0.025.DATimpatiens_set1_sub1.DAT impatiens_set1_sub0.8.DAT impatiens_set1_sub0.6.DAT impatiens_set1_sub0.4.DATimpatiens_set1_sub0.2.DAT impatiens_set1_sub0.1.DAT impatiens_set1_sub0.05.DAT impatiens_set1_sub0.025.DAT
+FILES=(augmented_pp0_poly.DAT augmented_pp0.DAT
+augmented_pp0.2_poly.DAT augmented_pp0.2.DAT
+augmented_pp0.4_poly.DAT augmented_pp0.4.DAT
+augmented_pp0.6_poly.DAT augmented_pp0.6.DAT
+augmented_pp0.8_poly.DAT augmented_pp0.8.DAT
+augmented_pp1_poly.DAT augmented_pp1.DAT
 )
 
 # Run COLONY on the file for this array task
