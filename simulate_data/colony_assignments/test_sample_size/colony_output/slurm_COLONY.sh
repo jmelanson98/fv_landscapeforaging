030d175f83d0e4cf84df216e822f6da95a9e6aeb
Jenna Melanson <melanson@gra-login1.graham.sharcnet>
2025-08-02 14:21:59 -0400

    save COLONY results

--- simulate_data/colony_assignments/test_sample_size/colony_output/slurm_COLONY.sh
+++ simulate_data/colony_assignments/test_sample_size/colony_output/slurm_COLONY.sh
@@ -17,7 +17,7 @@ mixtus_set2_sub1.DAT mixtus_set2_sub0.8.DAT mixtus_set2_sub0.6.DAT mixtus_set2_s
 mixtus_set2_sub0.2.DAT mixtus_set2_sub0.1.DAT mixtus_set2_sub0.05.DAT mixtus_set2_sub0.025.DAT
 mixtus_set1_sub1.DAT mixtus_set1_sub0.8.DAT mixtus_set1_sub0.6.DAT mixtus_set1_sub0.4.DAT
 mixtus_set1_sub0.2.DAT mixtus_set1_sub0.1.DAT mixtus_set1_sub0.05.DAT mixtus_set1_sub0.025.DAT
-impatiens_set3_sub1.DAT impatiens_set3_sub0.8.DAT impatiens_set3_sub0.6.DAT impatiens_set3_sub0.4.DATimpatiens_set3_sub0.2.DAT impatiens_set3_sub0.1.DAT impatiens_set3_sub0.05.DAT impatiens_set3_sub0.025.DATimpatiens_set2_sub1.DAT impatiens_set2_sub0.8.DAT impatiens_set2_sub0.6.DAT impatiens_set2_sub0.4.DATimpatiens_set2_sub0.2.DAT impatiens_set2_sub0.1.DAT impatiens_set2_sub0.05.DAT impatiens_set2_sub0.025.DATimpatiens_set1_sub1.DAT impatiens_set1_sub0.8.DAT impatiens_set1_sub0.6.DAT impatiens_set1_sub0.4.DATimpatiens_set1_sub0.2.DAT impatiens_set1_sub0.1.DAT impatiens_set1_sub0.05.DAT impatiens_set1_sub0.025.DAT
+impatiens_set3_sub1.DAT impatiens_set3_sub0.8.DAT impatiens_set3_sub0.6.DAT impatiens_set3_sub0.4.DAT impatiens_set3_sub0.2.DAT impatiens_set3_sub0.1.DAT impatiens_set3_sub0.05.DAT impatiens_set3_sub0.025.DAT impatiens_set2_sub1.DAT impatiens_set2_sub0.8.DAT impatiens_set2_sub0.6.DAT impatiens_set2_sub0.4.DAT impatiens_set2_sub0.2.DAT impatiens_set2_sub0.1.DAT impatiens_set2_sub0.05.DAT impatiens_set2_sub0.025.DAT impatiens_set1_sub1.DAT impatiens_set1_sub0.8.DAT impatiens_set1_sub0.6.DAT impatiens_set1_sub0.4.DAT impatiens_set1_sub0.2.DAT impatiens_set1_sub0.1.DAT impatiens_set1_sub0.05.DAT impatiens_set1_sub0.025.DAT
 )
 
 # Run COLONY on the file for this array task
