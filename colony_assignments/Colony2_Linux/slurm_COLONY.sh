8dec60bec9190361dcaaeee917469c8f1984c285
jmelanson98 <jenna.melanson@ubc.ca>
2025-08-21 21:51:37 -0700

    modify slurm script

--- colony_assignments/Colony2_Linux/slurm_COLONY.sh
+++ colony_assignments/Colony2_Linux/slurm_COLONY.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=colony_run
-#SBATCH --array=10-14
+#SBATCH --array=0-3
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=24G
 #SBATCH --time=24:00:00
@@ -11,10 +11,7 @@
 module load gcc
 
 # Your list of .DAT files (replace with real names or glob dynamically)
-FILES=(impatiens2022_final1.DAT impatiens2022_final2.DAT impatiens2022_final3.DAT impatiens2022_final4.DAT impatiens2022_final5.DAT
-impatiens2023_final1.DAT impatiens2023_final2.DAT impatiens2023_final3.DAT impatiens2023_final4.DAT impatiens2023_final5.DAT
-mixtus2022_final1.DAT mixtus2022_final2.DAT mixtus2022_final3.DAT mixtus2022_final4.DAT mixtus2022_final5.DAT
-mixtus2023_final1.DAT mixtus2023_final2.DAT mixtus2023_final3.DAT mixtus2023_final4.DAT mixtus2023_final5.DAT)
+FILES=(impatiens2022.DAT impatiens2023.DAT mixtus2022.DAT mixtus2023.DAT)
 
 # Run COLONY on the file for this array task
 INPUT=${FILES[$SLURM_ARRAY_TASK_ID]}
