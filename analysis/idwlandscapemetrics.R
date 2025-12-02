2f87caf2d02e2b5a625b7caf848ac55405445f66
jmelanson98 <jenna.melanson@ubc.ca>
2025-12-02 10:33:29 -0800

    working on calculating landscape metrics

--- analysis/idwlandscapemetrics.R
+++ analysis/idwlandscapemetrics.R
@@ -9,10 +9,15 @@ library(sf)
 library(raster)
 library(data.table)
 library(dbscan)
+library(tidyr)
+library(dplyr)
+library(bayesplot)
+library(ggplot2)
 
 # Set up workspace
 setwd("/Users/jenna1/fv_landscapeforaging")
 bombus_path = "/Users/jenna1/Documents/UBC/bombus_project/"
+#bombus_path = "home/projects/def-ckremen/melanson"
 source("src/analysis_functions.R")
 
 #####################################################
@@ -42,20 +47,110 @@ semi = rast(semi_file)
 #############################################
 # Get RHO values for each species x year
 #############################################
+# mixtus 2022
+stanFitm22 = readRDS("analysis/foraging_modelfits/foragingmodel_1.rds")
+summarym22 = rstan::summary(stanFitm22)$summary
+
+rhom22 = c(summarym22["rho", "2.5%"],
+           summarym22["rho", "mean"],
+           summarym22["rho", "97.5%"])
+rhom22 = rhom22*1000
+
+# mixtus 2023
+stanFitm23 = readRDS("analysis/foraging_modelfits/foragingmodel_2.rds")
+summarym23 = rstan::summary(stanFitm23)$summary
+
+rhom23 = c(summarym23["rho", "2.5%"],
+           summarym23["rho", "mean"],
+           summarym23["rho", "97.5%"])
+rhom23 = rhom23*1000
+
+
 # impatiens 2022
-stanFit = readRDS("analysis/foraging_modelfits/foragingmodel_3.rds")
-summary = summary(stanFit)$summary
+stanFiti22 = readRDS("analysis/foraging_modelfits/foragingmodel_3.rds")
+summaryi22 = rstan::summary(stanFiti22)$summary
 
-rho = c(summary["rho", "2.5%"],
-        summary["rho", "mean"],
-        summary["rho", "97.5%"])
-rho = rho*1000
+rhoi22 = c(summaryi22["rho", "2.5%"],
+           summaryi22["rho", "mean"],
+           summaryi22["rho", "97.5%"])
+rhoi22 = rhoi22*1000
 
+# impatiens 2023
+stanFiti23 = readRDS("analysis/foraging_modelfits/foragingmodel_4.rds")
+summaryi23 = rstan::summary(stanFiti23)$summary
+
+rhoi23 = c(summaryi23["rho", "2.5%"],
+           summaryi23["rho", "mean"],
+           summaryi23["rho", "97.5%"])
+rhoi23 = rhoi23*1000
+
+
+# Make plot of rhos for each species/year!
+postm22 = as.data.frame(stanFitm22)
+postm23 = as.data.frame(stanFitm23)
+posti22 = as.data.frame(stanFiti22)
+posti23 = as.data.frame(stanFiti23)
+
+combinedpost =  bind_rows(
+  data.frame(model = "B. mixtus 2022", rho = postm22$rho),
+  data.frame(model = "B. mixtus 2023", rho = postm23$rho),
+  data.frame(model = "B. impatiens 2022", rho = posti22$rho),
+  data.frame(model = "B. impatiens 2023", rho = posti23$rho)
+)
+
+ggplot(combinedpost, aes(x = rho, fill = model)) +
+  geom_histogram(alpha = 0.4) +
+  theme_minimal()
+
+year_difference = bind_rows(
+  data.frame(species = "mixtus", rho_diff = postm22$rho - postm23$rho),
+  data.frame(species = "impatiens", rho_diff = posti22$rho - posti23$rho)
+)
+
+ggplot(year_difference, aes(x = rho_diff, fill = species)) +
+  geom_histogram(alpha = 0.4) +
+  theme_minimal()
+
+mix_diff = year_difference[year_difference$species == "mixtus",]
+bayesp = sum(mix_diff$rho_diff > 0)/nrow(mix_diff)
+
+species_difference = bind_rows(
+  data.frame(year = "2022", rho_diff = posti22$rho - postm22$rho),
+  data.frame(year = "2023", rho_diff = posti23$rho - postm23$rho)
+)
+
+ggplot(species_difference, aes(x = rho_diff, fill = year)) +
+  geom_histogram(alpha = 0.4) +
+  theme_minimal()
 
 #############################################
 # Calculate IDW seminatural area per point
 #############################################
 buffer = 1500
+
+# for mixtus 2022
+fv_points$idwSN_mix22_low = NA
+fv_points$idwSN_mix22_mean = NA
+fv_points$idwSN_mix22_high = NA
+
+for (i in 1:nrow(fv_points)) {
+  fv_points$idwSN_mix22_low[i] = compute_idw_area(fv_points[i,], semi, buffer, rhom22[1])
+  fv_points$idwSN_mix22_mean[i] = compute_idw_area(fv_points[i,], semi, buffer, rhom22[2])
+  fv_points$idwSN_mix22_mean[i] = compute_idw_area(fv_points[i,], semi, buffer, rhom22[3])
+  if (i %% 10 == 0) cat("Processed point", i, "of", nrow(fv_points), "\n")
+}
+
+# for mixtus 2023
+fv_points$idwSN_mix23_low = NA
+fv_points$idwSN_mix23_mean = NA
+fv_points$idwSN_mix23_high = NA
+
+for (i in 1:nrow(fv_points)) {
+  fv_points$idwSN_mix23_low[i] = compute_idw_area(fv_points[i,], semi, buffer, rhom23[1])
+  #fv_points$idwSN_mix23_mean[i] = compute_idw_area(fv_points[i,], semi, buffer, rhom23[2])
+  #fv_points$idwSN_mix23_mean[i] = compute_idw_area(fv_points[i,], semi, buffer, rhom23[3])
+  if (i %% 10 == 0) cat("Processed point", i, "of", nrow(fv_points), "\n")
+}
 
 # for impatiens 2022
 fv_points$idwSN_imp22_low = NA
@@ -63,24 +158,136 @@ fv_points$idwSN_imp22_mean = NA
 fv_points$idwSN_imp22_high = NA
 
 for (i in 1:nrow(fv_points)) {
-    fv_points$idwSN_imp22_low[i] <- compute_idw_area(fv_points[i,], semi, buffer, rho[1])
-    fv_points$idwSN_imp22_mean[i] <- compute_idw_area(fv_points[i,], semi, buffer, rho[1])
-    fv_points$idwSN_imp22_mean[i] <- compute_idw_area(fv_points[i,], semi, buffer, rho[1])
+    #fv_points$idwSN_imp22_low[i] = compute_idw_area(fv_points[i,], semi, buffer, rhoi22[1])
+    fv_points$idwSN_imp22_mean[i] = compute_idw_area(fv_points[i,], semi, buffer, rhoi22[2])
+    #fv_points$idwSN_imp22_mean[i] = compute_idw_area(fv_points[i,], semi, buffer, rhoi22[3])
     if (i %% 10 == 0) cat("Processed point", i, "of", nrow(fv_points), "\n")
 }
 
-write.csv(fv_points, "analysis/calculated_metrics.csv")
+# for impatiens 2023
+fv_points$idwSN_imp23_low = NA
+fv_points$idwSN_imp23_mean = NA
+fv_points$idwSN_imp23_high = NA
+
+for (i in 1:nrow(fv_points)) {
+  fv_points$idwSN_imp23_low[i] = compute_idw_area(fv_points[i,], semi, buffer, rhoi23[1])
+  fv_points$idwSN_imp23_mean[i] = compute_idw_area(fv_points[i,], semi, buffer, rhoi23[2])
+  fv_points$idwSN_imp23_mean[i] = compute_idw_area(fv_points[i,], semi, buffer, rhoi23[3])
+  if (i %% 10 == 0) cat("Processed point", i, "of", nrow(fv_points), "\n")
+}
+st_write(fv_points, "analysis/calculated_metrics.shp")
 
 
 #############################################
 # Calculate IJI per point
 #############################################
+foraging_80_factor = sqrt(-2*log(1-0.8))
+
+# for mixtus 2022
+iji_mix22_min = calculateIJI(landcover.raster = landscape_raster, 
+                             site.shapefile = fv_points, 
+                             landcover.classification = landcover, 
+                             buffer.sizes = rhom22[1]*foraging_80_factor)
+iji_mix22_mean = calculateIJI(landcover.raster = landscape_raster, 
+                              site.shapefile = fv_points, 
+                              landcover.classification = landcover, 
+                              buffer.sizes = rhom22[2]*foraging_80_factor)
+iji_mix22_max = calculateIJI(landcover.raster = landscape_raster, 
+                             site.shapefile = fv_points, 
+                             landcover.classification = landcover, 
+                             buffer.sizes = rhom22[3]*foraging_80_factor)
+
+# for mixtus 2023
+iji_mix23_min = calculateIJI(landcover.raster = landscape_raster, 
+                             site.shapefile = fv_points, 
+                             landcover.classification = landcover, 
+                             buffer.sizes = rhom23[1]*foraging_80_factor)
+iji_mix23_mean = calculateIJI(landcover.raster = landscape_raster, 
+                              site.shapefile = fv_points, 
+                              landcover.classification = landcover, 
+                              buffer.sizes = rhom23[2]*foraging_80_factor)
+iji_mix23_max = calculateIJI(landcover.raster = landscape_raster, 
+                             site.shapefile = fv_points, 
+                             landcover.classification = landcover, 
+                             buffer.sizes = rhom23[3]*foraging_80_factor)
+
+
 
 # for impatiens 2022
-foraging_80_factor = sqrt(-2*log(1-0.8))
-iji_imp22 = calculateLandscapeMetrics(landcover.raster = landscape_raster, 
+iji_imp22_min = calculateIJI(landcover.raster = landscape_raster, 
                                       site.shapefile = fv_points, 
                                       landcover.classification = landcover, 
-                                      buffer.sizes = rho*foraging_80_factor)
+                                      buffer.sizes = rhoi22[1]*foraging_80_factor)
+iji_imp22_mean = calculateIJI(landcover.raster = landscape_raster, 
+                                          site.shapefile = fv_points, 
+                                          landcover.classification = landcover, 
+                                          buffer.sizes = rhoi22[2]*foraging_80_factor)
+iji_imp22_max = calculateIJI(landcover.raster = landscape_raster, 
+                                          site.shapefile = fv_points, 
+                                          landcover.classification = landcover, 
+                                          buffer.sizes = rhoi22[3]*foraging_80_factor)
+
+# for impatiens 2023
+iji_imp23_min = calculateIJI(landcover.raster = landscape_raster, 
+                             site.shapefile = fv_points, 
+                             landcover.classification = landcover, 
+                             buffer.sizes = rhoi23[1]*foraging_80_factor)
+iji_imp23_mean = calculateIJI(landcover.raster = landscape_raster, 
+                              site.shapefile = fv_points, 
+                              landcover.classification = landcover, 
+                              buffer.sizes = rhoi23[2]*foraging_80_factor)
+iji_imp23_max = calculateIJI(landcover.raster = landscape_raster, 
+                             site.shapefile = fv_points, 
+                             landcover.classification = landcover, 
+                             buffer.sizes = rhoi23[3]*foraging_80_factor)
 
 
+# for queen models
+iji_250 = calculateIJI(landcover.raster = landscape_raster, 
+                             site.shapefile = fv_points, 
+                             landcover.classification = landcover, 
+                             buffer.sizes = c(250))
+iji_1000 = calculateIJI(landcover.raster = landscape_raster, 
+                             site.shapefile = fv_points, 
+                             landcover.classification = landcover, 
+                             buffer.sizes = c(1000))
+
+
+
+#############################################
+# Calculate SN per point
+#############################################
+sn_250 = calculateSN(landcover.raster = landscape_raster, 
+                       site.shapefile = fv_points, 
+                       landcover.classification = landcover, 
+                       buffer.sizes = c(250))
+sn_1000 = calculateSN(landcover.raster = landscape_raster, 
+                        site.shapefile = fv_points, 
+                        landcover.classification = landcover, 
+                        buffer.sizes = c(1000))
+
+
+#############################################
+# Add to landscape metrics file
+#############################################
+saved = read.csv("analysis/calculated_metrics.csv")
+colnames(saved)[colnames(saved) == "X"] = "sample_pt"
+updated = saved %>%
+  left_join(iji_mix22_min) %>%
+  left_join(iji_mix22_mean) %>%
+  left_join(iji_mix22_max) %>%
+  left_join(iji_mix23_min) %>%
+  left_join(iji_mix23_mean) %>%
+  left_join(iji_mix23_max) %>%
+  left_join(iji_imp22_min) %>%
+  left_join(iji_imp22_mean) %>%
+  left_join(iji_imp22_max) %>%
+  left_join(iji_imp23_min) %>%
+  left_join(iji_imp23_mean) %>%
+  left_join(iji_imp23_max) %>%
+  left_join(iji_1000) %>%
+  left_join(iji_250) %>%
+  left_join(sn_250) %>%
+  left_join(sn_1000)
+
+write.csv(updated, "analysis/calculated_metrics.csv")
--- src/analysis_functions.R
+++ src/analysis_functions.R
@@ -7,10 +7,10 @@
 # Functions for calculating landscape metrics
 ##################################################
 
-calculateLandscapeMetrics <- function(landcover.raster, 
-                                      site.shapefile, 
-                                      landcover.classification, 
-                                      buffer.sizes){
+calculateIJI <- function(landcover.raster, 
+                         site.shapefile, 
+                         landcover.classification, 
+                         buffer.sizes){
   #this function will:
   #### - calculate area of different land classes in 500m buffer around each sample_pt
   #### - calculate landscape shdi in 500m buffer around each sample_pt
@@ -73,6 +73,69 @@ calculateLandscapeMetrics <- function(landcover.raster,
     #add prop_seminat and iji for given buffer size to landscape.dat
     #landscape.dat = left_join(landscape.dat, res1_seminat_summary, by = "sample_pt")
     landscape.dat = left_join(landscape.dat, res1_iji, by = "sample_pt")
+    
+    #update
+    print(paste("Buffer size", buffer, "done.", sep = " "))
+    
+  }
+  
+  return(landscape.dat)
+}
+
+
+
+calculateSN <- function(landcover.raster, 
+                         site.shapefile, 
+                         landcover.classification, 
+                         buffer.sizes){
+  #this function will:
+  #### - calculate area of different land classes in 500m buffer around each sample_pt
+  #### - calculate landscape shdi in 500m buffer around each sample_pt
+  #### - return a dataframe containing prop_blueberry, prop_edge, and shdi for each sample_pt
+  
+  # load packages
+  library(landscapemetrics)
+  
+  #set and change CRS
+  crs(landcover.raster) = 900913
+  st_crs(site.shapefile) = 900913
+  
+  # initiate landscape.dat dataframe
+  landscape.dat = as.data.frame(c(site.shapefile$site_id))
+  colnames(landscape.dat) = c("sample_pt")
+  
+  
+  for (buffer in buffer.sizes){
+    #area of each class in each buffer zone
+    res1_lsm_c_ca <- sample_lsm(landcover.raster,
+                                y = site.shapefile,
+                                plot_id = site.shapefile$site_id,
+                                shape = "circle",
+                                size = buffer,
+                                return_raster = TRUE,
+                                what = "lsm_c_ca")
+
+    #set class IDs
+    res1_lsm_c_ca = merge(res1_lsm_c_ca, landcover.classification[,c("lc_type", "class")], by = "class")
+    colnames(res1_lsm_c_ca)[colnames(res1_lsm_c_ca) == "plot_id"] = "sample_pt"
+
+    # get buffer areas
+    res1_lsm_c_ca %>%
+      group_by(sample_pt) %>%
+      summarise(buffer_area=sum(value)) -> buffer_areas
+    res1_lsm_c_ca = merge(res1_lsm_c_ca, buffer_areas, by = "sample_pt")
+    
+    # #calculate landscape seminatural area (hedgerow + blackberry + low edge + forest + wetland)
+    res1_seminat = filter(res1_lsm_c_ca, lc_type == "blackberry" | lc_type == "lowedge" | lc_type == "hedgerow" | lc_type == "forest" | lc_type == "wetland")
+    res1_seminat$split_prop = res1_seminat$value/res1_seminat$buffer_area
+    res1_seminat %>%
+      group_by(sample_pt) %>%
+      summarize(prop_seminat = sum(split_prop)) -> res1_seminat_summary
+    buffer_edge = paste("prop_seminat_", buffer, sep = "")
+    colnames(res1_seminat_summary) = c("sample_pt", buffer_edge)
+    
+    #add prop_seminat for given buffer size to landscape.dat
+    landscape.dat = left_join(landscape.dat, res1_seminat_summary, by = "sample_pt")
     
     #update
     print(paste("Buffer size", buffer, "done.", sep = " "))
