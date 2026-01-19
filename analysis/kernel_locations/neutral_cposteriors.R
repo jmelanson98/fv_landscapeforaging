632a45e1e55e7d918cffcd670403f529fd7f9d46
jmelanson98 <jenna.melanson@ubc.ca>
2026-01-19 00:53:12 -0800

    save simulation script

--- analysis/kernel_locations/neutral_cposteriors.R
+++ analysis/kernel_locations/neutral_cposteriors.R
@@ -25,8 +25,8 @@ library(terra)
 library(stringr)
 
 ##### Set Environment #####
-setwd("/Users/jenna1/fv_landscapeforaging")
-bombus_path = "/Users/jenna1/Documents/UBC/bombus_project"
+#setwd("/Users/jenna1/fv_landscapeforaging")
+#bombus_path = "/Users/jenna1/Documents/UBC/bombus_project"
 
 setwd("/home/melanson/projects/def-ckremen/melanson/fv_landscapeforaging")
 bombus_path = "/home/melanson/projects/def-ckremen/melanson"
@@ -36,7 +36,7 @@ source("src/analysis_functions.R")
  
 # Load in data / models
 mix.fit = readRDS("analysis/kernel_locations/foraging_modelfits/simpleforaging_mixtus_steepgradient.rds")
-#imp.fit = "analysis/kernel_locations/foraging_modelfits/simpleforaging_impatiens_steepgradient.rds"
+imp.fit = "analysis/kernel_locations/foraging_modelfits/simpleforaging_impatiens_steepgradient.rds"
 mixtus_sibs2022 = read.csv("data/siblingships/mixtus_sibships_2022.csv")
 mixtus_sibs2023 = read.csv("data/siblingships/mixtus_sibships_2023.csv")
 impatiens_sibs2022 = read.csv("data/siblingships/impatiens_sibships_2022.csv")
@@ -83,23 +83,23 @@ traps_m$trap_id = 1:nrow(traps_m)
 
 # Get rho draws from fitted model
 rhodraws_mix = rstan::extract(mix.fit, pars = "rho")$rho
-#rhodraws_imp = rstan::extract(mix.fit, pars = "rho")$rho
+rhodraws_imp = rstan::extract(mix.fit, pars = "rho")$rho
 mix_rho = rhodraws_mix[task_id*4000/100]
-#imp_rho = rhodraws_imp[task_id*4000/100]
+imp_rho = rhodraws_imp[task_id*4000/100]
 
 # Simulate one dataset
-mixsim = draw_simple_true_sites(sample_size = 2000,
-                          number_colonies = 11502,
-                          colony_sizes = rep(20,11502),
+mixsim = draw_simple_true_sites(sample_size = 6000,
+                          number_colonies = 18000,
+                          colony_sizes = rep(20,18000),
                           trap_data = traps_m,
                           rho = mix_rho,
                           distance_decay = "exponentiated_quadratic")
-# impsim = draw_simple_true_sites(sample_size = 4000,
-#                                 number_colonies = 8004,
-#                                 colony_sizes = rep(20,8004),
-#                                 trap_data = traps_m,
-#                                 rho = imp_rho,
-#                                 distance_decay = "exponentiated_quadratic")
+impsim = draw_simple_true_sites(sample_size = 6000,
+                               number_colonies = 18000,
+                               colony_sizes = rep(20,18000),
+                               trap_data = traps_m,
+                               rho = imp_rho,
+                               distance_decay = "exponentiated_quadratic")
 
 # Get colony counts (real data)
 samplepoints = read.csv(paste0(bombus_path, "/raw_data/allsamplepoints.csv"), header = FALSE)
@@ -122,10 +122,6 @@ mixcounts_summary = mixtus_data[[2]] %>%
 mixsimcounts = mixsim %>% 
   group_by(colonyid) %>%
   summarize(n = sum(counts)) %>%
-  group_by(n) %>%
-  summarize(numperbin = n())
-  
-  
   filter(n %in% mixcounts_summary$n) %>%
   group_by(n) %>%
   tidyr::nest() %>%
@@ -136,22 +132,22 @@ mixsimIDs_tokeep = map2(mixsimcounts$data, mixsimcounts$num_per_bin,
 mixsim_tokeep = mixsim[mixsim$colonyid %in% mixsimIDs_tokeep$colonyid,]
 
 
-# impcounts_summary = imptus_data[[2]] %>% 
-#   group_by(stansibkey) %>%
-#   summarize(n = sum(count)) %>%
-#   group_by(n) %>%
-#   summarize(num_per_bin = n())
-# impsimcounts = impsim %>% 
-#   group_by(colonyid) %>%
-#   summarize(n = sum(counts)) %>%
-#   filter(n %in% impcounts_summary$n) %>%
-#   group_by(n) %>%
-#   tidyr::nest() %>%
-#   left_join(impcounts_summary, by = "n")
-# impsimIDs_tokeep = map2(impsimcounts$data, impsimcounts$num_per_bin,
-#                         ~ slice_sample(.x, n = .y, replace = FALSE)) %>%
-#   bind_rows()
-# impsim_tokeep = impsim[impsim$colonyid %in% impsimIDs_tokeep$colonyid,]
+impcounts_summary = imptus_data[[2]] %>%
+  group_by(stansibkey) %>%
+  summarize(n = sum(count)) %>%
+  group_by(n) %>%
+  summarize(num_per_bin = n())
+impsimcounts = impsim %>%
+  group_by(colonyid) %>%
+  summarize(n = sum(counts)) %>%
+  filter(n %in% impcounts_summary$n) %>%
+  group_by(n) %>%
+  tidyr::nest() %>%
+  left_join(impcounts_summary, by = "n")
+impsimIDs_tokeep = map2(impsimcounts$data, impsimcounts$num_per_bin,
+                        ~ slice_sample(.x, n = .y, replace = FALSE)) %>%
+  bind_rows()
+impsim_tokeep = impsim[impsim$colonyid %in% impsimIDs_tokeep$colonyid,]
 
 
 # Write to file
@@ -159,6 +155,6 @@ mixdir = paste0("analysis/kernel_locations/neutral_data/mixdataset", task_id)
 dir.create(mixdir, recursive = TRUE, showWarnings = FALSE)
 write.csv(mixsim_tokeep, paste0(mixdir, "/simulation", task_id, ".csv"), row.names = FALSE)
 
-# impdir = paste0("analysis/kernel_locations/neutral_data/impdataset", task_id)
-# dir.create(impdir, recursive = TRUE, showWarnings = FALSE)
-# write.csv(impsim_tokeep, paste0(impdir, "/simulation", task_id, ".csv"), row.names = FALSE)
+impdir = paste0("analysis/kernel_locations/neutral_data/impdataset", task_id)
+dir.create(impdir, recursive = TRUE, showWarnings = FALSE)
+write.csv(impsim_tokeep, paste0(impdir, "/simulation", task_id, ".csv"), row.names = FALSE)
--- analysis/kernel_locations/slurm.sh
+++ analysis/kernel_locations/slurm.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-##SBATCH --job-name=simpleforaging
-#SBATCH --output=logs/simpleforaging_%A_%a.out
-#SBATCH --error=logs/simpleforaging_%A_%a.err
-#SBATCH --array=1-2
+##SBATCH --job-name=workersim
+#SBATCH --output=logs/workersim_%A_%a.out
+#SBATCH --error=logs/workersim_%A_%a.err
+#SBATCH --array=1-100
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=30G
-#SBATCH --time=08:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=8G
+#SBATCH --time=01:00:00
 
 module load StdEnv/2023 gcc r/4.3.1 gdal proj
 
@@ -14,4 +14,4 @@ module load StdEnv/2023 gcc r/4.3.1 gdal proj
 export R_LIBS_USER=$HOME/R/x86_64-pc-linux-gnu-library/4.3
 
 
-Rscript simple_foraging.R
+Rscript neutral_cposteriors.R
